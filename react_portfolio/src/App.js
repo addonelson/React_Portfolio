edc427efc4b1d461e31b3a784c52b9a911861837
Aaron Donelson <aaron.donelson23@gmail.com>
2021-11-13 12:47:17 -0500

    adjusted app css

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import React from "react";
 import './App.css';
+import Home from './components/landing';
+import Navigation from './components/navbar';
+import Container from "react-bootstrap/Container";
+import { About, Work, Contact } from "./components/pages"
+import Footer from './components/footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="positioning">
+      <HashRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="work" element={<Work />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+
+      </HashRouter>
     </div>
-  );
+    <Footer />
+  </div>
+);
 }
 
 export default App;
